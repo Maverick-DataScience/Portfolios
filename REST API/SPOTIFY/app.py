365c70af4ce2ffcc88336ac24a07593fbf82cf45
Maverick-DataScience <109931488+Maverick-DataScience@users.noreply.github.com>
2024-08-07 18:13:34 +1000

    Update app.py

--- REST API/SPOTIFY/app.py
+++ REST API/SPOTIFY/app.py
@@ -1,5 +1,5 @@
 # Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
-const token = 'BQDrmO579WYNFezwN9zEygMjuXS46WuwEyvkvw8Inba7cOwCBXtLB8UHHkMvfpIO7fychSkYkuRZ5hJF4bvz1rOXguJuw6H_PqMeGXb2mQTJ5lu23BNoveFPoo_Ae9xGNTQXVJoFPoZcf5W1bvnRWg2vff1BXou91ysdjPwYm8S7I2yLggvirBW_RakRBbkWmOZIA4EGseWessy4EoSdiFrc4KfIHQfvxNMDI-4iVbnGT4VRQE7FH_aSCJGXoCUt3j4T3Xbl5XG0JRdbPw';
+const token = ;
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
